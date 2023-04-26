86e896d06683cf929e1336bc8bb8a52f5800bdd3
NamalTharindu97 <NamalA701997@gmail.com>
2023-04-26 20:33:47 +0530

    order history

--- frontend/src/screens/OrderHistoryScreen.js
+++ frontend/src/screens/OrderHistoryScreen.js
@@ -8,6 +8,7 @@ import { Store } from '../Store';
 import { getError } from '../utils';
 import Button from 'react-bootstrap/esm/Button';
 
+// Define the reducer function
 const reducer = (state, action) => {
   switch (action.type) {
     case 'FETCH_REQUEST':
@@ -21,26 +22,36 @@ const reducer = (state, action) => {
   }
 };
 
+// Define the component
 export default function OrderHistoryScreen() {
+  // Get the user info from the global state
   const { state } = useContext(Store);
   const { userInfo } = state;
+
+  // Get the navigate function from the react-router-dom library
   const navigate = useNavigate();
 
+  // Use the reducer hook to manage the state
   const [{ loading, error, orders }, dispatch] = useReducer(reducer, {
     loading: true,
     error: '',
   });
+
+  // Fetch the order history data when the component mounts
   useEffect(() => {
     const fetchData = async () => {
+      // Dispatch a "FETCH_REQUEST" action to set the loading state
       dispatch({ type: 'FETCH_REQUEST' });
       try {
+        // Make a GET request to the server to fetch the order history data
         const { data } = await axios.get(
           `http://localhost:5000/api/orders/mine`,
-
           { headers: { Authorization: `Bearer ${userInfo.token}` } }
         );
+        // Dispatch a "FETCH_SUCCESS" action with the fetched data
         dispatch({ type: 'FETCH_SUCCESS', payload: data });
       } catch (error) {
+        // Dispatch a "FETCH_FAIL" action with the error message
         dispatch({
           type: 'FETCH_FAIL',
           payload: getError(error),
@@ -49,6 +60,7 @@ export default function OrderHistoryScreen() {
     };
     fetchData();
   }, [userInfo]);
+
   return (
     <div>
       <Helmet>
